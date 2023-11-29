a74760b686d1f563ae6ca5da72265b474e4337a4
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-11-29 00:10:36 -0700

    Added player node and basic movement
    added template movement script and walk/sprint speed variables

---
+++ Scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+
+var current_speed = 5.0
+
+@export var walking_speed = 5.0
+@export var sprinting_speed = 10.0
+@export var crouching_speed = 3.0
+
+var jump_velocity = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("sprint"):
+		current_speed = sprinting_speed
+	else:
+		current_speed = walking_speed
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
+	move_and_slide()
