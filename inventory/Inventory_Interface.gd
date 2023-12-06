ab1a608961e9d9f46fefa63ce9e21b41e28a082a
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-05 19:22:53 -0700

    Added function to move item within inventory
    can pick up, but currently can't put it back down.

--- inventory/Inventory_Interface.gd
+++ inventory/Inventory_Interface.gd
@@ -1,9 +1,28 @@
 extends Control
+
+var grabbed_slot_data: SlotData
+
 @onready var player_inventory = $PlayerInventory
+@onready var grabbed_slot = $GrabbedSlot
+
+func _physics_process(delta: float) -> void:
+	if grabbed_slot.visible:
+		grabbed_slot.global_position = get_global_mouse_position() + Vector2(5, 5)
 
 func set_player_inventory_data(inventory_data: InventoryData) -> void:
 	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
 	
 func on_inventory_interact(inventory_data: InventoryData, index: int, button:int) -> void:
-	print("%s %s %s" % [inventory_data, index, button])
+	#print("%s %s %s" % [inventory_data, index, button])
+	match [grabbed_slot_data, button]:
+		[null, MOUSE_BUTTON_LEFT]:
+			grabbed_slot_data = inventory_data.grab_slot_data(index)
+	update_grabbed_slot()
+
+func update_grabbed_slot() -> void:
+	if grabbed_slot_data:
+		grabbed_slot.show()
+		grabbed_slot.set_slot_data(grabbed_slot_data)
+	else:
+		grabbed_slot.hide()
--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -5,6 +5,14 @@ signal inventory_interact(inventory_data: InventoryData, index: int, button:int)
 
 @export var slot_datas: Array[SlotData]
 
+func grab_slot_data(index: int) -> SlotData:
+	var slot_data = slot_datas[index]
+	
+	if slot_data:
+		return slot_data
+	else:
+		return null
+
 func on_slot_clicked(index: int, button:int) -> void:
 	print("Inventory Interact")
 	inventory_interact.emit(self, index, button)
