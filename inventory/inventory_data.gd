f330336da4f123806c082b5f7d608ed864e1cff3
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-05 22:49:08 -0700

    Added can_fully_merge_with and fully_merge_with functions to allow stackable items of the same type to be stacked together if there is space in the stack
    If you have 3 apples and 4 apples you can combine them into a stack of 7. You cannot stack above the maximum size limit of 99,

--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -15,9 +15,14 @@ func grab_slot_data(index: int) -> SlotData:
 
 func drop_slot_data(grabbed_slot_data: SlotData, index: int) -> SlotData:
 	var slot_data = slot_datas[index]
-	slot_datas[index] = grabbed_slot_data
+	var return_slot_data: SlotData
+	if slot_data and slot_data.can_fully_merge_with(grabbed_slot_data):
+		slot_data.fully_merge_with(grabbed_slot_data)
+	else:
+		slot_datas[index] = grabbed_slot_data
+		return_slot_data = slot_data
 	inventory_updated.emit(self)
-	return slot_data
+	return return_slot_data
 
 
 func on_slot_clicked(index: int, button:int) -> void:
