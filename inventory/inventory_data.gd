3975278d0cea21e9fffd07767e439dd15f05822f
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-06 12:49:21 -0700

    If you have a stack of an item that you are trying to pick up and also have available capacity in your stack of items, the picked up item with merge with the existing stack
    Otherwise, it will take a new slot

--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -39,10 +39,17 @@ func drop_single_slot_data(grabbed_slot_data: SlotData, index: int) -> SlotData:
 func pick_up_slot_data(slot_data: SlotData) -> bool:
 	
 	for index in slot_datas.size():
+		if slot_datas[index] and slot_datas[index].can_fully_merge_with(slot_data):
+			slot_datas[index].fully_merge_with(slot_data)
+			inventory_updated.emit(self)
+			return true
+
+	for index in slot_datas.size():
 		if not slot_datas[index]:
 			slot_datas[index] = slot_data
 			inventory_updated.emit(self)
 			return true
+			
 	return false
 
 func on_slot_clicked(index: int, button:int) -> void:
