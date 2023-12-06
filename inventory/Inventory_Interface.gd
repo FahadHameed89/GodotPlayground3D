5229a07c19cd996cf7fb6b60ed924579d4b13a17
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-05 18:54:12 -0700

    Added inventory interact signal

--- inventory/Inventory_Interface.gd
+++ inventory/Inventory_Interface.gd
@@ -2,4 +2,8 @@ extends Control
 @onready var player_inventory = $PlayerInventory
 
 func set_player_inventory_data(inventory_data: InventoryData) -> void:
+	inventory_data.inventory_interact.connect(on_inventory_interact)
 	player_inventory.set_inventory_data(inventory_data)
+	
+func on_inventory_interact(inventory_data: InventoryData, index: int, button:int) -> void:
+	print("%s %s %s" % [inventory_data, index, button])
--- inventory/inventory_data.gd
+++ inventory/inventory_data.gd
@@ -1,7 +1,10 @@
 extends Resource
 class_name InventoryData
 
+signal inventory_interact(inventory_data: InventoryData, index: int, button:int)
+
 @export var slot_datas: Array[SlotData]
 
 func on_slot_clicked(index: int, button:int) -> void:
 	print("Inventory Interact")
+	inventory_interact.emit(self, index, button)
