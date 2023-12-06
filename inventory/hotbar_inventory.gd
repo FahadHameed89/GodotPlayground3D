0784f58d02e960421947a4d8802f52319942debc
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-06 14:19:50 -0700

    Hotbar can now be used
    Pressing keys 1 though 5 will activate consumable items which are in the associated item slot

--- inventory/hotbar_inventory.gd
+++ inventory/hotbar_inventory.gd
@@ -1,12 +1,22 @@
 extends PanelContainer
 
+signal hot_bar_use(index: int)
+
 const Slot = preload("res://inventory/slot.tscn")
 
 @onready var h_box_container = $MarginContainer/HBoxContainer
 
+func _unhandled_key_input(event: InputEvent) -> void:
+	if not visible or not event.is_pressed():
+		return
+		
+	if range(KEY_1, KEY_6).has(event.keycode):
+		hot_bar_use.emit(event.keycode - KEY_1)
+
 func set_inventory_data(inventory_data: InventoryData) -> void:
 	inventory_data.inventory_updated.connect(populate_hot_bar)
 	populate_hot_bar(inventory_data)
+	hot_bar_use.connect(inventory_data.use_slot_data)
 
 func populate_hot_bar(inventory_data: InventoryData) -> void:
 	for child in h_box_container.get_children():
