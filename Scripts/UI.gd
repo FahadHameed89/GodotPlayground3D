753d851719dfee2be747397a6199f8edc0f0a0a0
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-09 22:28:25 -0700

    Bravery variables connected in ui script
    bravery variables now directly affect  the bravery ui element

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 @onready var hp_bar = $StatBars/VitalStats/HPBar
 @onready var mp_bar = $StatBars/VitalStats/MPBar
 @onready var sp_bar = $StatBars/VitalStats/SPBar
+@onready var bravery_bar = $StatBars/VitalStats/RadialMeters/BraveryBar
 
 
 
@@ -15,6 +16,8 @@ func _ready():
 	mp_bar.value = player.current_mana
 	sp_bar.max_value = player.max_stamina
 	sp_bar.value = player.current_stamina
+	bravery_bar.max_value = player.max_bravery
+	bravery_bar.value = player.current_bravery
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -25,3 +28,4 @@ func UpdateHealth():
 	hp_bar.value = player.current_health
 	mp_bar.value = player.current_mana
 	sp_bar.value = player.current_stamina
+	bravery_bar.value = player.current_bravery
