ba95b01e8179e49a92f480c827d953cb5ed4e88f
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-09 17:04:29 -0700

    Updated item_data_consumable resource to also affect MP and SP
    no items implement this feature yet

--- item/item_data_consumable.gd
+++ item/item_data_consumable.gd
@@ -3,9 +3,22 @@ class_name ItemDataConsumable
 
 @export var heal_value: int
 @export var damage_value: int
+@export var mp_heal_value: int
+@export var mp_cost_value: int
+@export var sp_heal_value: int
+@export var sp_cost_value: int
 
 func use(target) -> void:
 	if heal_value != 0:
 		target.heal(heal_value)
+	if mp_heal_value != 0:
+		target.mp_heal(mp_heal_value)
+	if sp_heal_value != 0:
+		target.sp_heal(sp_heal_value)
+		
 	if damage_value != 0:
 		target.damage(damage_value)
+	if mp_cost_value != 0:
+		target.mp_cost(mp_cost_value)
+	if mp_cost_value != 0:
+		target.sp_cost(sp_cost_value)
