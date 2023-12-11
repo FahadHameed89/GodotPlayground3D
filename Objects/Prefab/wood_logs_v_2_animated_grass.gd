b5a1972b75ae3acc530a2c56d6a200f5b2ecd2d3
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-11 15:46:53 -0700

    Added an animated glb file for testing purposes
    I can only make one animation work at a time...

---
+++ Objects/Prefab/wood_logs_v_2_animated_grass.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@onready var animation_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation_player.play("EmptyAction_002")
+
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
