85d947efe446d9d31f7017f7b219bd265a155292
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-11 13:56:30 -0700

    All derived stats now update when stats are allocated

--- Scripts/character_sheet.gd
+++ Scripts/character_sheet.gd
@@ -135,19 +135,27 @@ func LoadStats():
 	
 	player_critical_chance_value = player.critical_chance
 	critical_value.set_text(str(player_critical_chance_value))
+	
 	player_evasion_chance_value = player.evasion_chance
+	evasion_value.set_text(str(player_evasion_chance_value))
 	
 	player_action_speed_value = player.action_speed
+	action_speed_value.set_text(str(player_action_speed_value))
 	
 	player_magic_attack_value = player.magic_attack
+	magic_attack_value.set_text(str(player_magic_attack_value))
 	
 	player_magic_defense_value = player.magic_defense
+	magic_defense_value.set_text(str(player_magic_defense_value))
 	
 	player_minion_power_value = player.minion_power
+	minion_power_value.set_text(str(player_minion_power_value))
 	
 	player_effect_duration_value = player.effect_duration
+	effect_duration_value.set_text(str(player_effect_duration_value))
 	
 	player_cast_speed_value = player.cast_speed
+	cast_speed_value.set_text(str(player_cast_speed_value))
 
 func CalculateDerivedStats():
 	player.physical_attack = (player.strength * 2) + (player.dexterity * 1)
