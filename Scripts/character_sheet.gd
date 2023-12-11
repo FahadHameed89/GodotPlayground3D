d3d2782f8adcb71fc05223581c634b9d41810083
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-10 17:35:03 -0700

    Wired up vitality plus buttons

--- Scripts/character_sheet.gd
+++ Scripts/character_sheet.gd
@@ -11,18 +11,29 @@ var path_main_stats = "HBoxContainer/VBoxContainer/HBoxContainer/MainStats"
 
 @onready var vitality_change = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Vitality/StatsBackground/Stats/VitalityChange
 @onready var vitality_value = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Vitality/StatsBackground/Stats/VitalityValue
+@onready var vitality_minus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Vitality/StatsBackground/VitalityMinus
+@onready var vitality_plus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Vitality/StatsBackground/VitalityPlus
 
 @onready var dexterity_change = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Dexterity/StatsBackground/Stats/DexterityChange
 @onready var dexterity_value = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Dexterity/StatsBackground/Stats/DexterityValue
+@onready var dexterity_minus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Dexterity/StatsBackground/DexterityMinus
+@onready var dexterity_plus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Dexterity/StatsBackground/DexterityPlus
 
 @onready var agility_change = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Agility/StatsBackground/Stats/AgilityChange
 @onready var agility_value = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Agility/StatsBackground/Stats/AgilityValue
+@onready var agility_minus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Agility/StatsBackground/AgilityMinus
+@onready var agility_plus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Agility/StatsBackground/AgilityPlus
 
 @onready var spirit_change = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Spirit/StatsBackground/Stats/SpiritChange
 @onready var spirit_value = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Spirit/StatsBackground/Stats/SpiritValue
+@onready var spirit_minus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Spirit/StatsBackground/SpiritMinus
+@onready var spirit_plus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Spirit/StatsBackground/SpiritPlus
 
 @onready var charisma_change = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Charisma/StatsBackground/Stats/CharismaChange
 @onready var charisma_value = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Charisma/StatsBackground/Stats/CharismaValue
+@onready var charisma_minus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Charisma/StatsBackground/CharismaMinus
+@onready var charisma_plus = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/Charisma/StatsBackground/CharismaPlus
+
 
 @onready var stat_points_label = $HBoxContainer/VBoxContainer/HBoxContainer/MainStats/HBoxContainer/StatPointsLabel
 var available_points = 5
@@ -60,7 +71,14 @@ func _on_strength_plus_pressed():
 
 func _on_vitality_plus_pressed():
 	print("Vitality + 1")
-
+	vit_add += 1 # Increase the stat in code
+	vitality_change.set_text("+ " + str(vit_add)) # update the change label
+	vitality_minus.disabled = false # unlock the minus button
+	available_points -= 1 # reduce the available stat points in code
+	stat_points_label.set_text(str(available_points)) # reduce the avaiable stat points on the label
+	if available_points == 0: # if available points = 0, lock all plus buttons
+		for button in get_tree().get_nodes_in_group("PlusButtons"):
+			button.set_disabled(true)
 
 func _on_dexterity_plus_pressed():
 	print("Dexterity + 1")
