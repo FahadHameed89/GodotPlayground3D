dc9a2f715da0470bd9d81e8368ab72a044344604
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-13 23:18:34 -0700

    set up actionable script
    for a better interaction system

---
+++ helpers/actionable.gd
@@ -0,0 +1,7 @@
+extends Area3D
+
+@export var dialogue_resource: DialogueResource 
+@export var dialogue_start: String = "start"
+
+func action() -> void:
+	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
