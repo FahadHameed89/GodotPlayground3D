525fb569b28caa994e90e3ca4386a6f86c1489c1
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-17 14:34:03 -0700

    Setting up weapon resources

---
+++ Scripts/weapon_resource.gd
@@ -0,0 +1,18 @@
+extends Resource
+
+class_name  Weapon_Resource
+
+@export var Weapon_Name: String
+@export var Activate_Anim: String
+@export var Shoot_Anim: String
+@export var Reload_Anim: String
+@export var Deactivate_Anim: String
+@export var Out_Of_Ammo_Anim: String
+
+@export var Current_Ammo: int
+@export var Reserve_Ammo: int
+@export var Magazine: int
+@export var Max_Ammo: int
+
+@export var Auto_Fire: bool
+
