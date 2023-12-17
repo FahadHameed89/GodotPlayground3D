6782c028fae1bc183913c9b1be3b2cd4195cf8a3
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-17 15:02:25 -0700

    Created weapon resource examples and started weapon manager script

---
+++ Scripts/Weapons_Manager.gd
@@ -0,0 +1,39 @@
+extends Node3D
+
+@onready var weapon_animation_player = get_node("%WeaponAnimationPlayer")
+
+var Current_Weapon = null
+
+var Weapon_Stack = [] # An array of weapons currently held by the player
+
+var Weapon_Indicator = 0
+
+var Next_Weapon: String
+
+var Weapon_List = {}
+
+@export var _weapon_resources: Array[Weapon_Resource]
+
+@export var Start_Weapons: Array[String]
+
+func _ready():
+	Initialize(Start_Weapons) # Enter the state machine
+
+func Initialize(_start_weapons: Array):
+	for weapon in _weapon_resources:
+		Weapon_List[weapon.Weapon_Name] = weapon
+		
+	for i in _start_weapons:
+		Weapon_Stack.push_back(i) # Add out start weapons
+	
+func enter():
+	# When we enter into a weapon state
+	pass
+	
+	
+func exit():
+	# In order to change weapons, first call exit
+	pass
+	
+func Change_Weapon():
+	pass
