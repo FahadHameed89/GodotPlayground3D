a3219dfd37f699fdb6eaf5c9391094ddc3685328
Fahad Hameed <69335527+FahadHameed89@users.noreply.github.com>
2023-12-17 23:48:54 -0700

    Added impulse physics to hitscan projectiles
    They apply a slight impulse which is increased with higher damage

--- Objects/physics_target.gd
+++ Objects/physics_target.gd
@@ -3,8 +3,13 @@ extends RigidBody3D
 var Health = 10
 
 func Hit_Successful(Damage, _Direction:= Vector3.ZERO, _Position:= Vector3.ZERO):
+
+	var Hit_Position = _Position - get_global_transform().origin
+
 	Health -= Damage
 	print("Target HP: " + str(Health))
-	
 	if Health <= 0:
 		queue_free()
+
+	if _Direction != Vector3.ZERO:
+		apply_impulse((_Direction*Damage),Hit_Position)
